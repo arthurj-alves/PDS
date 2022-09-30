4e41661e87a978ed854703f3bff25696761dfcd3
Arthur Jorge <arthurjorgeac@gmail.com>
2022-09-30 13:41:25 -0300

    third commit

---
+++ Lista4.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+//#include <math.h>
+
+int fat(int n){
+    if (n == 0) return 1;
+    else return (n*fat(n-1));
+}
+
+int mdc(int a, int b){
+    if (b == 0) return a;   
+    return mdc(b, a % b);
+}
+
+int mdc3(int a, int b, int c){
+    return (mdc(a, mdc(b, c)));
+}
+
+int fib(int n){
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int primo (int x, int n){
+    if (n==1) return (1);
+    if (x%n==0) return(0);
+    return (primo(x, n-1)); 
+}
+
+int decrescente(int n){      //Não está funcionando
+    if(n>0){
+        printf("%d\n", n);
+        return (decrescente(n-1));
+    }
+    return 0;
+}
+
+int res(int a, int b){
+    return (a%b);
+}
+
+int form(int n){
+    if(n>0) return(n*n + form(n-1));
+    return 0;
+}
+
+int mmc(int a, int b){
+    return ((a*b) / mdc(a, b));
+}
+
+int div(int a, int b){
+    return (a/b);
+}
+
+float qrd(float x, float chute ){
+    if (chute*chute >=-0.001 && chute*chute <= x+0.001) return (chute);
+    if (chute*chute > x) return (raiz(x, chute*0.5));
+    if (chute*chute < x) return (raiz(x, chute*1.5));
+}
+
+int dig(int n){
+    int unid, dez, cent;
+    cent = n / 100;
+    dez = (n - (cent*100)) / 10;
+    unid = (n - (cent*100) - (dez*10)); 
+    return (cent + dez + unid);
+}
+
+int main(){
+    
+    printf("%d", mdc(23, 7));
+
+    return 0;
+}
+
